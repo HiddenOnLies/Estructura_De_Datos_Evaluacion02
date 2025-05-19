80893e2edad12208f6fe8672f39b15be14e4cfc2
LuchitoAlberto <luchoalbertomanzur@gmail.com>
2025-05-19 19:34:24 -0400

    Update Ejercicio02.c

--- Ejercicio02.c
+++ Ejercicio02.c
@@ -14,13 +14,15 @@ typedef struct Pila {
     Codigo* cabeza;
 } Pila;
 
-//Prototipos de funciones
+// Prototipo de funciones
 void inicializar(Pila * compartimento);
 bool estavacia(Pila * compartimento);
 void push(Pila * compartimento, int codigo);
-int pop(Pila * compartimento);
+void pop(Pila * compartimento);
+int top(Pila * compartimento);
 void repartir(Pila *a, Pila *b, Pila *c, int codigos[]);
 void reensamblar(Pila * a, Pila * b, Pila * c, int codigosnuevos[]);
+void adivinar(Pila * b);
 void liberar(Pila * compartimento);
 void mostrar_uno(Pila * compartimento);
 void mostrar(Pila * a, Pila * b, Pila * c);
@@ -30,7 +32,7 @@ void inicializar(Pila * compartimento) {
     compartimento -> cabeza = NULL;
 }
 
-// Verficamos si es que un compartimento esta vacio
+// Verficamos si es que el compartimento esta vacio
 bool estavacia(Pila * compartimento) {
     return compartimento -> cabeza == NULL;
 }
@@ -47,20 +49,18 @@ void push(Pila * compartimento, int codigo) {
     compartimento -> cabeza = nuevo;
 }
 
-// Eliminamos la cabeza del compartimento correspodiente
-int pop(Pila * compartimento) {
+// Eliminamos y retornamos la cabeza del compartimento correspodiente
+void pop(Pila * compartimento) {
     if (estavacia(compartimento)) {
         printf("Error: Compartimento vacio\n");
-        return 0;
+        return;
     }
     Codigo * actual = compartimento -> cabeza;
-    int codigo = actual -> cifras;
     compartimento -> cabeza = actual -> siguiente;
-    free(actual);   
-    return codigo;
+    free(actual);
 }
 
-// Obtenemos la cabeza del compartimento correspondiente
+// Retornamos la cabeza del compartimento correspondiente
 int top(Pila * compartimento) {
     if (estavacia(compartimento)) {
         printf("Error: Compartimento vacio\n");
@@ -87,53 +87,73 @@ void repartir(Pila *a, Pila *b, Pila *c, int codigos[]) {
 void reensamblar(Pila * a, Pila * b, Pila * c, int codigosnuevos[]) {
     char eleccion;
     do {
-        printf("En que compartimento esta su codigo: ('a', 'b', o 'c'): ");
+        printf("¿En que compartimento esta su codigo? ('a', 'b', o 'c'): ");
         scanf(" %c", &eleccion);
         if (eleccion != 'a' && eleccion != 'b' && eleccion != 'c') {
-            printf("Error: Eleccion no valida. Intente nuevamente\n");
+            printf("Error: Eleccion no valida. Intente nuevamente...\n");
         }
     } while (eleccion != 'a' && eleccion != 'b' && eleccion != 'c');
     int i = 0;
     if (eleccion == 'a') {
         while (!estavacia(b)) {
-            codigosnuevos[i] = pop(b);
+            codigosnuevos[i] = top(b);
+            pop(b);
             i += 1;
         }
         while (!estavacia(a)) {
-            codigosnuevos[i] = pop(a);
+            codigosnuevos[i] = top(a);
+            pop(a);
             i += 1;
         }
         while (!estavacia(c)) {
-            codigosnuevos[i] = pop(c);
+            codigosnuevos[i] = top(c);
+            pop(c);
             i += 1;
         }
     } else if (eleccion == 'b') {
         while (!estavacia(a)) {
-            codigosnuevos[i] = pop(a);
+            codigosnuevos[i] = top(a);
+            pop(a);
             i += 1;
         }
         while (!estavacia(b)) {
-            codigosnuevos[i] = pop(b);
+            codigosnuevos[i] = top(b);
+            pop(b);
             i += 1;
         }
         while (!estavacia(c)) {
-            codigosnuevos[i] = pop(c);
+            codigosnuevos[i] = top(c);
+            pop(c);
             i += 1;
         }
     } else {
         while (!estavacia(a)) {
-            codigosnuevos[i] = pop(a);
+            codigosnuevos[i] = top(a);
+            pop(a);
             i += 1;
         }
         while (!estavacia(c)) {
-            codigosnuevos[i] = pop(c);
+            codigosnuevos[i] = top(c);
+            pop(c);
             i += 1;
         }
         while (!estavacia(b)) {
-            codigosnuevos[i] = pop(b);
+            codigosnuevos[i] = top(b);
+            pop(b);
             i += 1;
         }
     }
+}
+
+// Identificamos el codigo que el usuario eligio
+void adivinar(Pila * b) {
+    Codigo * actual = b -> cabeza;
+    int codigoresultado;
+    for (int i = 0; i < 3; i++) {
+        actual = actual -> siguiente;
+    }
+    codigoresultado = actual -> cifras;
+    printf("Su codigo objetivo es: %02d\n\n", codigoresultado);
 }
 
 // Liberamos la memoria del sistema mediante la eliminacion de un compartimento
@@ -146,25 +166,25 @@ void liberar(Pila * compartimento) {
 // Mostramos el contenido del compartimento correspondiente
 void mostrar_uno(Pila * compartimento) {
     if (estavacia(compartimento)) {
-        printf("Error: Compartimento Vacio\n");
+        printf("%-15s", "(Vacío)");
         return;
     }
-    Codigo * actual = compartimento -> cabeza;
+    Codigo * actual = compartimento->cabeza;
     while (actual != NULL) {
-        printf("%d\n", actual -> cifras);
-        actual = actual -> siguiente;
+        printf("%02d ", actual->cifras);
+        actual = actual->siguiente;
     }
+    printf("\n");
 }
 
 // Mostramos el contenido de cada compartimento
 void mostrar(Pila * a, Pila * b, Pila * c) {
-    printf("\nCompartimento A:\n");
+    printf("\n");
+    printf("A: ");
     mostrar_uno(a);
-    printf("\n");
-    printf("Compartimento B:\n");
+    printf("B: ");
     mostrar_uno(b);
-    printf("\n");
-    printf("Compartimento C:\n");
+    printf("C: ");
     mostrar_uno(c);
     printf("\n");
 }
@@ -176,15 +196,18 @@ int main() {
 
     int codigos[CANTIDADCODIGOS] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21};
     repartir(&a, &b, &c, codigos);
+
+    printf("\nBienvenido señor agente!!\n");
+    printf("Seleccione uno de los 21 codigos que tiene a disposicion y memorizelo: \n");
     mostrar(&a, &b, &c);
 
     int codigosnuevos[CANTIDADCODIGOS];
-
     for (int i = 0; i < 3; i++) {
         reensamblar(&a, &b, &c, codigosnuevos);
         repartir(&a, &b, &c, codigosnuevos);
         mostrar(&a, &b, &c);
     }
+    adivinar(&b);
 
     liberar(&a);
     liberar(&b);
